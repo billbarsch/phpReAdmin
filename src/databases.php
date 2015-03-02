ae7517fd348155717d473f8252fb5958703ef2f9
billbarsch <billbarsch@gmail.com>
2015-03-02 03:04:49 -0300

    one more modal form

--- src/databases.php
+++ src/databases.php
@@ -34,12 +34,12 @@ if($_SESSION["host_address"]=='')
 	$modal_show = 'show';
 
 ?>
-        <div class="modal <?php echo $modal_show;?>" id="myModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal <?php echo $modal_show;?>" id="host_modal" role="dialog" aria-labelledby="host_modalLabel" aria-hidden="true">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Rethinkdb Host address</h4>
+                <h4 class="modal-title" id="host_modalLabel">Rethinkdb Host address</h4>
               </div>
               <div class="modal-body">
                   <form id="InfroText" action="<?php echo $_SERVER['PHP_SELF'];?>?action=config" method="POST">
@@ -58,6 +58,28 @@ if($_SESSION["host_address"]=='')
 <?php
 if($_SESSION["host_address"]!==''){
 ?>
+         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Create database</h4>
+              </div>
+              <div class="modal-body">
+                  <form id="InfroText" action="<?php echo $_SERVER['PHP_SELF'];?>?database=new&action=new_database" method="POST">
+                      <div class="form-group">
+                          <label for="database_name">Database name</label>
+                          <input type="text" name="database_name" id="database_name" required>
+                      </div>
+                  <div style="text-align:right">    
+                  <button type="submit" class="btn btn-primary">create database</button>
+                  </div>
+                  </form>
+              </div>
+            </div>
+          </div>
+        </div>
+         
          <h1 class="page-header">Databases (<?php echo $_SESSION["host_address"];?>)</h1>
           <!-- <h2 class="sub-header"></h2> -->
          <a data-toggle="modal" data-target="#myModal" class="btn btn-primary" role="button">+ new database</a>
