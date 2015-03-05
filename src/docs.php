3f8e3a32f842e7e3fbd2eb42bf6a74a12c9444ab
billbarsch <billbarsch@gmail.com>
2015-03-05 09:26:50 -0300

    optimize one row json viewer

--- src/docs.php
+++ src/docs.php
@@ -28,18 +28,31 @@ if($_REQUEST["action"]=="delete_doc"){
               </thead>
               <tbody>
 				<?php
-				
+				function first_keys($array){
+					$result = array();
+					foreach(array_keys($array) as $key){
+						if(is_array($array[$key])||is_object($array[$key])){
+							//nada
+						}else{
+							$result[$key] = $array[$key];
+						}
+					}
+					return $result;
+				}//first keys
                 $docs = r\db($_REQUEST["database"])->table($_REQUEST["table"])->run($conn);
 				$docs = $docs->toNative();
                 foreach($docs as $doc){
-				?>
-                <tr>
-                  <td><a href="<?php echo $_SERVER['PHP_SELF'];?>?database=<?php echo $_REQUEST["database"];?>&table=<?php echo $_REQUEST["table"];?>&doc=<?php echo $doc["id"];?>"><?php echo substr(json_encode($doc,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),0,100);?></a></td>
-                  <td><a href="<?php echo $_SERVER['PHP_SELF'];?>?database=<?php echo $_REQUEST["database"];?>&table=<?php echo $_REQUEST["table"];?>&doc=<?php echo $doc["id"];?>&action=delete_doc" class="btn btn-danger" role="button">delete</a></td>
-                  <td></td>
-                </tr>
-                <?php
-                }//each databases
+					$doc = first_keys($doc);
+                	$doc_text = json_encode($doc,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+					$doc_text = substr($doc_text,0,100);
+					?>
+	                <tr>
+	                  <td><a href="<?php echo $_SERVER['PHP_SELF'];?>?database=<?php echo $_REQUEST["database"];?>&table=<?php echo $_REQUEST["table"];?>&doc=<?php echo $doc["id"];?>"><?php echo $doc_text;?></a></td>
+	                  <td><a href="<?php echo $_SERVER['PHP_SELF'];?>?database=<?php echo $_REQUEST["database"];?>&table=<?php echo $_REQUEST["table"];?>&doc=<?php echo $doc["id"];?>&action=delete_doc" class="btn btn-danger" role="button">delete</a></td>
+	                  <td></td>
+	                </tr>
+	                <?php
+                }//each doc
                 ?>
 
               </tbody>
